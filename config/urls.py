92efa40a5dabea2ce11236fc9c1780bc4d5c494d
tominarit <tominarit@gmail.com>
2021-03-09 08:52:08 +0900

    initial commit

---
+++ config/urls.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+# from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blog API",
+        default_version="v1",
+        description="A sample API for learning DRF",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="sample@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('posts.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0), name='schema-redoc'),
+    # path('openapi', get_schema_view(
+    #     title="Blog API",
+    #     description="A sample API for learning DRF",
+    #     version="1.0.0"
+    # ), name='openapi-schema'),
+]
---
+++ posts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+from .views import UserViewSet, PostViewSet
+# from .views import UserList, UserDetail, PostList, PostDetail
+
+
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+    # path('users/', UserList.as_view()),
+    # path('users/<int:pk>/', UserDetail.as_view()),
+    # path('<int:pk>/', PostDetail.as_view()),
+    # path('', PostList.as_view()),
+# ]
